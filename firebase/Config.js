a8fdc4f8e5be8bef0e0d4c55fbe5b88c0ef499b0
Hajax8 <t2luha01@students.oamk.fi>
2024-03-05 19:25:57 +0200

    Update Config.js

--- firebase/Config.js
+++ firebase/Config.js
@@ -3,12 +3,7 @@ import { getFirestore, serverTimestamp, collection, addDoc } from 'firebase/fire
 import { getAuth, signInWithEmailAndPassword } from 'firebase/auth'
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDYwwaXv_o9bdrG0MA3_Gdv3ZDOPijOmHU",
-    authDomain: "chat-d227a.firebaseapp.com",
-    projectId: "chat-d227a",
-    storageBucket: "chat-d227a.appspot.com",
-    messagingSenderId: "716102977391",
-    appId: "1:716102977391:web:ecac103a95bf5831d84a9a"
+
   };
 
   initializeApp(firebaseConfig);
